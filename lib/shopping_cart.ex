79378d628f61f9f79ce4d17dbbe67e9a3d6832c2
Virgilio Lino <virgilio@riskident.com>
2017-03-25 23:56:14 +0100

    fix on itemId field, now its called itemI instead of id because of compatibility

--- lib/shopping_cart.ex
+++ lib/shopping_cart.ex
@@ -7,17 +7,17 @@ defmodule ShoppingCart do
 	acc + String.to_float(Map.get(elem, "pricePerUnit")) * String.to_integer(Map.get(elem, "quantity"))
       end
     )
-    isDiscounted = Enum.any?(cart, fn {_, elem} -> Map.get(elem, "id") in Map.values(discountedItemIds) end)
+    isDiscounted = Enum.any?(cart, fn {_, elem} -> Map.get(elem, "itemId") in Map.values(discountedItemIds) end)
     return_value(total, isDiscounted, discount)
   end
 
-  
+
   defp return_value(total, true, discount) do
     total * discount
   end
 
   defp return_value(total, false, discount) do
-    total 
-  end  
-    
+    total
+  end
+  
 end
